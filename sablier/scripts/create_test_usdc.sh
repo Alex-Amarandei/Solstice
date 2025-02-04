0efb9902f93e057d08d029d218cbd0a7c998a824
Alex Amarandei <alex.m.amarandei@gmail.com>
2025-02-04 23:22:56 +0200

    :hammer: fix(mint): fix config file

--- sablier/scripts/create_test_usdc.sh
+++ sablier/scripts/create_test_usdc.sh
@@ -3,6 +3,10 @@
 # solana-keygen grind --starts-with mnt:1
 # This resulted in mntwikCmYs2USRwa9TtAqeJR1r9rc3mvaQbmbQmsFo7.json
 
-spl-token create-token --program-id TokenzQdBNbLqP5VEhdkAS6EPFLC1PHnBqCXEpPxuEb --enable-metadata mntwikCmYs2USRwa9TtAqeJR1r9rc3mvaQbmbQmsFo7.json
+spl-token create-token --program-id TokenzQdBNbLqP5VEhdkAS6EPFLC1PHnBqCXEpPxuEb --enable-metadata mntPMTbkKfo4ht5ccjsxqAXKapEqu84on5mxsJCwfdf.json
 
-spl-token initialize-metadata mntwikCmYs2USRwa9TtAqeJR1r9rc3mvaQbmbQmsFo7 "Test USDC" "USDC" ""
+spl-token initialize-metadata mntPMTbkKfo4ht5ccjsxqAXKapEqu84on5mxsJCwfdf "Test USDC" "USDC" "https://raw.githubusercontent.com/Alex-Amarandei/Solstice/refs/heads/main/sablier/scripts/test_usdc.json"
+
+spl-token create-account mntPMTbkKfo4ht5ccjsxqAXKapEqu84on5mxsJCwfdf
+
+spl-token mint mntPMTbkKfo4ht5ccjsxqAXKapEqu84on5mxsJCwfdf 1000000
